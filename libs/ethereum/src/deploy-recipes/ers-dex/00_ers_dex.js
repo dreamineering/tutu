86e192acd51423c4e568d4774989889846afc87f
Matt Mischewski <matt@dreamineering.com>
2022-05-11 11:14:42 +0200

    Add dex contracts

---
+++ libs/ethereum/src/deploy-recipes/ers-dex/00_ers_dex.js
@@ -0,0 +1,63 @@
+// deploy/00_deploy_your_contract.js
+
+const { ethers } = require("hardhat");
+
+const localChainId = "31337";
+
+// const sleep = (ms) =>
+//   new Promise((r) =>
+//     setTimeout(() => {
+//       console.log(`waited for ${(ms / 1000).toFixed(3)} seconds`);
+//       r();
+//     }, ms)
+//   );
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const chainId = await getChainId();
+  const { deployer } = await getNamedAccounts();
+
+  // await deploy("Balloons", {
+  //   from: deployer,
+  //   log: true,
+  // });
+
+  const balloons = await ethers.getContract("Balloons", deployer);
+
+  await deploy("DEX", {
+    from: deployer,
+    args: [balloons.address],
+    log: true,
+    waitConfirmations: 5,
+  });
+
+  const dex = await ethers.getContract("DEX", deployer);
+
+  // paste in your front-end address here to get 10 balloons on deploy:
+  // scaffold eth wallet
+  // await balloons.transfer(
+  //   "0xfE2f43CEd86Bfbd2D2Fb310923267567A39cD1f2",
+  //   "" + 10 * 10 ** 18
+  // );
+
+  // metamask wallet
+  await balloons.transfer(
+    "0x0C0f72931ea185Ca04dc7b18C15348688bB8d94B",
+    "" + 10 * 10 ** 18
+  );
+
+  // uncomment to init DEX on deploy:
+  console.log(
+    "Approving DEX (" + dex.address + ") to take Balloons from main account..."
+  );
+
+  console.log("Deployer Address", deployer);
+  // If you are going to the testnet make sure your deployer account has enough ETH
+  await balloons.approve(dex.address, ethers.utils.parseEther("0.01"));
+  console.log("INIT exchange...");
+  await dex.init(ethers.utils.parseEther("0.01"), {
+    value: ethers.utils.parseEther("0.01"),
+    gasLimit: 200000,
+  });
+};
+module.exports.tags = ["Balloons", "DEX"];
